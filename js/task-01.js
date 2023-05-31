5ff07cc157910305338df2efcd6cf9543dcb7e3b
Serhii Kondratenko <k.serjio@ukr.net>
2023-06-01 00:11:40 +0300

    ++

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+const bodyEl = document.body;
+
+const listEl = document.querySelectorAll(".item");
+
+console.log(`Number of categories : ${listEl.length}`);
+
+console.log(bodyEl.h2)
+
+// listEl.forEach( element => console.log(element))
+
+// console.log(listEl);
