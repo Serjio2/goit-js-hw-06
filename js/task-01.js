38e31ceee8f66c731320acb1a1ed4a545b12a7b2
Serhii Kondratenko <kondratenko.serhii3@gmail.com>
2023-06-01 18:25:13 +0300

    ++

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,18 @@
-const bodyEl = document.body;
 
-const listEl = document.querySelectorAll(".item");
+const listEl = document.querySelector("#categories");
 
-console.log(`Number of categories : ${listEl.length}`);
+console.log(`Number of categories : ${listEl.children.length}`);
 
-console.log(bodyEl.h2)
+// console.log(listEl.children)
+// console.log(itemsEl)
 
-// listEl.forEach( element => console.log(element))
+const titleEl = listEl.children;
+console.log(titleEl);
 
-// console.log(listEl);
+// titleEl.forEach( element => 
+//     console.log(element.children.textContent))
+
+// const titleEl = document.querySelectorAll("h2");
+// titleEl.forEach(elem => console.log(elem.textContent))
+// console.log(titleEl);
+
