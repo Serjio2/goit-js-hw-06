bd853b061ce2b987bcf1a3768d672c754e3bb553
Serhii Kondratenko <kondratenko.serhii3@gmail.com>
2023-06-06 17:16:42 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,43 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const controlsDivEl = document.querySelector("#controls");
+const inputDivEl = controlsDivEl.firstElementChild;
+const createBtnEl = inputDivEl.nextElementSibling;
+const destroyBtnEl = controlsDivEl.lastElementChild;
+
+const boxesDiv = document.querySelector("#boxes");
+
+
+// console.log(controlsDivEl);
+// console.log(inputDivEl.number);
+// console.log(createBtnEl);
+// console.log(destroyBtnEl);
+// console.log(boxesDiv);
+
+createBtnEl.addEventListener("click", () => {
+  const numberDivs = document.createElement("div");
+  inputDivEl.map((value) => numberDivs(value));
+  numberDivs.style.width = "30px";
+  numberDivs.style.height = "30px";
+  numberDivs.style.backgroundColor = "red";
+
+  console.log(inputDivEl.value);
+
+
+  boxesDiv.insertAdjacentElement("beforeend", numberDivs);
+  
+  // console.log(inputDivEl.value);
+  // console.log(numberDivs);
+  console.log(boxesDiv)
+} )
+
+// console.log(document)
+
+
+
+// function createBoxes(amount) {
+//   console.log(inputDivEl);
+// }
